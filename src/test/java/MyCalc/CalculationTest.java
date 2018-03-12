d0d9bc6f2d9c7b191e5e8540a77b0b8867f57aec
svjat32 <svjat32@gmail.com>
2018-03-12 23:32:39 +0300

    cs

--- src/test/java/MyCalc/CalculationTest.java
+++ src/test/java/MyCalc/CalculationTest.java
@@ -10,15 +10,15 @@ public class CalculationTest {
         Assert.assertEquals(10, calculation.sum(2,8));
     }
     @Test
-    public void testdiff() throws Exception{
+    public void testDiff() throws Exception{
         Assert.assertEquals(3,calculation.diff(5,2));
     }
     @Test
-    public void testmult() throws Exception{
+    public void testMult() throws Exception{
         Assert.assertEquals(6,calculation.mult(3,2));
     }
     @Test
-    public void testdev() throws Exception{
+    public void testDev() throws Exception{
         Assert.assertEquals(12,calculation.dev(24,2));
     }
 }
